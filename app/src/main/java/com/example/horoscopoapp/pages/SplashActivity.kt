63e6a5002b00b15fff75126f7bc388dc8433d967
Antonio PG <antpergom.prof@gmail.com>
2024-03-22 21:44:38 +0100

    Latest changes

---
+++ app/src/main/java/com/example/horoscopoapp/pages/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.horoscopoapp.pages
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceBundel : Bundle?) {
+        super.onCreate(savedInstanceBundel)
+        //setContentView(R.layout.activity_splash)
+
+    }
+}
